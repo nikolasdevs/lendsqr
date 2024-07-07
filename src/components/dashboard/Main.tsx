99a54247f1773fe6d1369a7b8566f3d5a29d1e8c
Emmanuel Esan <nikolasdevs@gmail.com>
2024-07-07 15:01:45 +0100

    png images replaced by sgv images. filter function also added

--- src/components/dashboard/Main.tsx
+++ src/components/dashboard/Main.tsx
@@ -1,32 +1,49 @@
 import { useEffect, useState } from "react";
-import iconBox1 from "../../assets/box-icon1.png";
-import iconBox2 from "../../assets/box-icon2.png";
-import iconBox3 from "../../assets/box-icon3.png";
-import iconBox4 from "../../assets/box-icon4.png";
-import UserData from "../UserData";
+import iconBox1 from "../../assets/usersIcon.svg";
+import iconBox2 from "../../assets/usersIcon2.svg";
+import iconBox3 from "../../assets/usersIcon3.svg";
+import iconBox4 from "../../assets/usersIcon4.svg";
+import UserData from "../user/UserData";
 import DataHeader from "./DataHeader";
 import ReactPaginate from "react-paginate";
 import { addUserToDB, getUsersFromDB, clearUsersFromDB } from "./Db";
+import UserFilter from "../user/UserFilter";
 
 interface User {
   id: number;
   organization: string;
   username: string;
   email: string;
-  phoneNumber: string;
+  phoneNum: string;
   dateJoined: string;
-  status: string;
+  status: "active" | "inactive" | "pending" | "blacklisted";
   hasLoan: boolean;
   hasSavings: boolean;
 }
 
 const Main = () => {
   const [userData, setUserData] = useState<User[]>([]);
-
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
   const usersPerPage = 10;
   const [itemOffset, setItemOffset] = useState(0);
+  const [filters, setFilters] = useState<{
+    organization: string;
+    username: string;
+    email: string;
+    date: string;
+    phoneNumber: string;
+    status: string;
+  }>({
+    organization: "",
+    username: "",
+    email: "",
+    date: "",
+    phoneNumber: "",
+    status: "",
+  });
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+  const [activeUsersCount, setActiveUsersCount] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,7 +54,7 @@ const Main = () => {
           setLoading(false);
         } else {
           const response = await fetch(
-            "https://run.mocky.io/v3/931bbc55-66eb-45b5-b3ba-44fa3025c0fd"
+            "https://run.mocky.io/v3/9d1249cf-7b7b-439a-b436-602c3bcee2cd"
           );
           const data = await response.json();
           await clearUsersFromDB();
@@ -54,7 +71,17 @@ const Main = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    // Calculate the number of active users
+    const calculateActiveUsers = () => {
+      const activeCount = userData.filter(
+        (user) => user.status === "active"
+      ).length;
+      setActiveUsersCount(activeCount);
+    };
 
+    calculateActiveUsers();
+  }, [userData]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -64,8 +91,42 @@ const Main = () => {
     return <div>Error: {error.message}</div>;
   }
 
+  const filteredUsers = userData.filter((user) => {
+    const organizationMatch = filters.organization
+      ? user.organization
+          .toLowerCase()
+          .includes(filters.organization.toLowerCase())
+      : true;
+    const usernameMatch = filters.username
+      ? user.username.toLowerCase().includes(filters.username.toLowerCase())
+      : true;
+    const emailMatch = filters.email
+      ? user.email.toLowerCase().includes(filters.email.toLowerCase())
+      : true;
+    const dateMatch = filters.date
+      ? user.dateJoined.toLowerCase().includes(filters.date.toLowerCase())
+      : true;
+    const phoneNumberMatch = filters.phoneNumber
+      ? user.organization
+          .toLowerCase()
+          .includes(filters.phoneNumber.toLowerCase())
+      : true;
+    const statusMatch = filters.status
+      ? user.status.toLowerCase().includes(filters.status.toLowerCase())
+      : true;
+
+    return (
+      organizationMatch &&
+      usernameMatch &&
+      emailMatch &&
+      dateMatch &&
+      phoneNumberMatch &&
+      statusMatch
+    );
+  });
+
   const endOffset = itemOffset + usersPerPage;
-  const currentUsers = userData.slice(itemOffset, endOffset);
+  const currentUsers = filteredUsers.slice(itemOffset, endOffset);
   const pageCount = Math.ceil(userData.length / usersPerPage);
 
   const handleDropdownChange = (
@@ -76,10 +137,22 @@ const Main = () => {
 
   const handlePageClick = (event: { selected: number }) => {
     const newOffset = (event.selected * usersPerPage) % userData.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
     setItemOffset(newOffset);
+  };
+
+  const toggleFilterPopup = () => {
+    setIsFilterOpen(!isFilterOpen);
+  };
+
+  const handleFilterApply = (filters: {
+    organization: string;
+    username: string;
+    email: string;
+    date: string;
+    phoneNumber: string;
+    status: string;
+  }) => {
+    setFilters(filters);
   };
 
   return (
@@ -95,9 +168,7 @@ const Main = () => {
         <div className="title-box">
           <img src={iconBox2} alt="" />
           <p>ACTIVE USERS</p>
-          <p className="fig">
-            {userData.filter((user) => user.status === "active").length}
-          </p>
+          <p className="fig">{activeUsersCount}</p>
         </div>
         <div className="title-box">
           <img src={iconBox3} alt="" />
@@ -117,28 +188,68 @@ const Main = () => {
 
       <div className="dataBox">
         <div className="users-headers">
-          <DataHeader title="ORGANIZATION" className="org" />
-          <DataHeader title="USERNAME" className="usr" />
-          <DataHeader title="EMAIL" className="email" />
-          <DataHeader title="PHONE NUMBER" className="num" />
-          <DataHeader title="DATE JOINED" className="date" />
-          <DataHeader title="STATUS" className="status" />
+          <DataHeader
+            title="ORGANIZATION"
+            className="org"
+            onFilterClick={toggleFilterPopup}
+            isFilterOpen={isFilterOpen}
+          />
+          <DataHeader
+            title="USERNAME"
+            className="usr"
+            onFilterClick={toggleFilterPopup}
+            isFilterOpen={isFilterOpen}
+          />
+          <DataHeader
+            title="EMAIL"
+            className="email"
+            onFilterClick={toggleFilterPopup}
+            isFilterOpen={isFilterOpen}
+          />
+          <DataHeader
+            title="PHONE NUMBER"
+            className="num"
+            onFilterClick={toggleFilterPopup}
+            isFilterOpen={isFilterOpen}
+          />
+          <DataHeader
+            title="DATE JOINED"
+            className="date"
+            onFilterClick={toggleFilterPopup}
+            isFilterOpen={isFilterOpen}
+          />
+          <DataHeader
+            title="STATUS"
+            className="status"
+            onFilterClick={toggleFilterPopup}
+            isFilterOpen={isFilterOpen}
+          />
         </div>
 
-        {currentUsers.map((user) => (
-          <UserData
-            key={user.id}
-            organization={user.organization}
-            username={user.username}
-            email={user.email}
-            phoneNumber={user.phoneNumber}
-            dateJoined={user.dateJoined}
-            status={"active"}
+        {isFilterOpen && (
+          <UserFilter
+            isOpen={isFilterOpen}
+            onApply={handleFilterApply}
+            onClose={toggleFilterPopup}
           />
+        )}
+        {currentUsers.map((user) => (
+          <div>
+            <UserData
+              key={user.id}
+              id={user.id}
+              organization={user.organization}
+              username={user.username}
+              email={user.email}
+              phoneNumber={user.phoneNum}
+              dateJoined={user.dateJoined}
+              status={user.status}
+            />
+          </div>
         ))}
       </div>
+
       <div className="pagination-container">
-        {" "}
         <div className="select-box">
           <p>Showing </p>
           <div className="select">
@@ -152,6 +263,7 @@ const Main = () => {
           </div>
           <p> out of {userData.length}</p>
         </div>
+
         <ReactPaginate
           breakLabel="..."
           nextLabel=">"
